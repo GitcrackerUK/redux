d6df3a45014875dfea567a939cffc39d12afdce4
pawel.siwek <pawel.siwek@filament.uk.com>
2021-04-15 10:42:37 +0100

    Added post&postform components to app

--- traversy/src/App.js
+++ traversy/src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Post from './components/Post'
+import Post from './components/Post';
+import PostForm from './components/Postform';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      
-      </header>
-      <Post/>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+            </header>
+            <PostForm></PostForm>
+            <Post />
+        </div>
+    );
 }
 
 export default App;
